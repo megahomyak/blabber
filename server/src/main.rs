5260b545e5ffb3a3916f29bdf174dcc8bce43670
megahomyak <g.megahomyak@gmail.com>
2025-02-12 02:09:31 +0500

    Yay, fixed

--- server/src/main.rs
+++ server/src/main.rs
@@ -21,43 +21,34 @@ fn main() {
     .unwrap();
     let ass = std::sync::Mutex::new(ass);
     let server = rouille::Server::new("0.0.0.0:80", move |request| {
-        println!("{:?}", ass.try_lock().map(|_ass| ()));
-        let mut ass = ass.lock().unwrap();
         let mut data = String::new();
         request.data().unwrap().read_to_string(&mut data).unwrap();
         let request: Request = serde_json::from_str(&data).unwrap();
 
         let mut new_messages = Vec::new();
-        let mut current_message_id: u64 = request.last_message_id.unwrap_or(0);
-        println!("Usage 1");
+        let mut current_message_id: u64 = request.last_message_id.map(|id| id + 1).unwrap_or(0);
         while let Some(new_message) =
-            ass.get(format!("{}/messages/{}", request.room_name, current_message_id).as_bytes())
+            ass.lock().unwrap().get(format!("{}/messages/{}", request.room_name, current_message_id).as_bytes())
         {
-            println!("Usage 1 in progress");
             let new_message: serde_json::Value =
                 serde_json::from_str(&String::from_utf8(new_message).unwrap()).unwrap();
-            current_message_id = new_message.get("id").unwrap().as_u64().unwrap();
+            current_message_id += 1;
             new_messages.push(new_message);
         }
-        println!("Usage 1 end");
 
         let mut self_message_success = None;
         if !request.self_message_lines.is_empty() {
-            println!("Usage 2");
             let new_message_id =
-                ass.get(format!("{}/last message id", request.room_name).as_bytes());
-            println!("Usage 2 end");
+                ass.lock().unwrap().get(format!("{}/last message id", request.room_name).as_bytes());
             let new_message_id: u64 = new_message_id.map_or_else(
                 || 0,
                 |id| String::from_utf8(id).unwrap().parse::<u64>().unwrap() + 1,
             );
             let new_message_id_string = new_message_id.to_string();
-            println!("Usage 3");
-            ass.set(
+            ass.lock().unwrap().set(
                 format!("{}/last message id", request.room_name).as_bytes(),
                 new_message_id_string.as_bytes(),
             );
-            println!("Usage 3 end");
             let utc_unix_timestamp = chrono::Utc::now().timestamp();
             let new_message = serde_json::json!({
                 "lines": request.self_message_lines,
@@ -65,18 +56,15 @@ fn main() {
                 "utc_unix_timestamp": utc_unix_timestamp,
                 "sender_name": request.self_name,
             });
-            println!("Usage 4");
-            ass.set(
+            ass.lock().unwrap().set(
                 format!("{}/messages/{}", request.room_name, new_message_id).as_bytes(),
                 new_message.to_string().as_bytes(),
             );
-            println!("Usage 4 end");
             self_message_success = Some(serde_json::json!({
                 "utc_unix_timestamp": utc_unix_timestamp,
                 "id": new_message_id,
             }));
         }
-        drop(ass);
         rouille::Response::text(
             serde_json::json!({
                 "self_message_success": self_message_success,
