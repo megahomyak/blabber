59271597f45d5948a34d0b12d1e0385048f81507
megahomyak <g.megahomyak@gmail.com>
2025-02-11 15:34:58 +0500

    Where the fuck do you deadlock, buddy...

--- server/src/main.rs
+++ server/src/main.rs
@@ -20,41 +20,34 @@ fn main() {
     )
     .unwrap();
     let ass = std::sync::Mutex::new(ass);
-    rouille::start_server("0.0.0.0:80", move |request| {
+    let server = rouille::Server::new("0.0.0.0:80", move |request| {
         println!("{}", ass.try_lock().is_ok());
+        let mut ass = ass.lock().unwrap();
         let mut data = String::new();
         request.data().unwrap().read_to_string(&mut data).unwrap();
         let request: Request = serde_json::from_str(&data).unwrap();
 
         let mut new_messages = Vec::new();
         let mut current_message_id: u64 = request.last_message_id.unwrap_or(0);
-        while let Some(new_message) = ass
-            .lock()
-            .unwrap()
-            .get(format!("{}/messages/{}", request.room_name, current_message_id).as_bytes())
+        while let Some(new_message) =
+            ass.get(format!("{}/messages/{}", request.room_name, current_message_id).as_bytes())
         {
             let new_message: serde_json::Value =
                 serde_json::from_str(&String::from_utf8(new_message).unwrap()).unwrap();
-            current_message_id = new_message
-                .get("id")
-                .unwrap()
-                .as_u64()
-                .unwrap();
+            current_message_id = new_message.get("id").unwrap().as_u64().unwrap();
             new_messages.push(new_message);
         }
 
         let mut self_message_success = None;
         if !request.self_message_lines.is_empty() {
-            let new_message_id = ass
-                .lock()
-                .unwrap()
-                .get(format!("{}/last message id", request.room_name).as_bytes());
+            let new_message_id =
+                ass.get(format!("{}/last message id", request.room_name).as_bytes());
             let new_message_id: u64 = new_message_id.map_or_else(
                 || 0,
                 |id| String::from_utf8(id).unwrap().parse::<u64>().unwrap() + 1,
             );
             let new_message_id_string = new_message_id.to_string();
-            ass.lock().unwrap().set(
+            ass.set(
                 format!("{}/last message id", request.room_name).as_bytes(),
                 new_message_id_string.as_bytes(),
             );
@@ -65,7 +58,7 @@ fn main() {
                 "utc_unix_timestamp": utc_unix_timestamp,
                 "sender_name": request.self_name,
             });
-            ass.lock().unwrap().set(
+            ass.set(
                 format!("{}/messages/{}", request.room_name, new_message_id).as_bytes(),
                 new_message.to_string().as_bytes(),
             );
@@ -81,5 +74,12 @@ fn main() {
             })
             .to_string(),
         )
+    }).unwrap();
+    let (join_handle, stopper) = server.stoppable();
+    std::thread::spawn(move || {
+        for _sig in signal_hook::iterator::Signals::new([signal_hook::consts::SIGTERM, signal_hook::consts::SIGINT, signal_hook::consts::SIGQUIT]).unwrap().forever() {
+            stopper.send(()).unwrap();
+        }
     });
+    join_handle.join().unwrap();
 }
